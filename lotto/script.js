c0047cd1b9cf1dc06b07284eba0f7f9b3128c31d
Robert Kapala <Robert.Kapala@gmail.com>
2020-06-09 22:34:06 +0200

    Update output name

--- lotto/script.js
+++ lotto/script.js
@@ -8,9 +8,10 @@ function init(){
   }
   
   function handleFileSelect(event) {
-    const reader = new FileReader()
+    document.getElementById('uploadedFileName').textContent = event.target.files[0].name
+    const reader = new FileReader();
     reader.onload = handleFileLoad;
-    reader.readAsText(event.target.files[0])
+    reader.readAsText(event.target.files[0]);
   }
 
   function isNotBlank(input) {
@@ -31,42 +32,30 @@ function download(filename, text) {
   }
   
   function handleFileLoad(event){
-    console.log("handleFIleLoad...")
-    var lines = event.target.result.split(/\r?\n/)
-    var nonEmptyLines = lines.filter(isNotBlank)
-    console.log( document.getElementById('rowsNumber'));
-    document.getElementById('rowsNumber').textContent = lines.length
-    document.getElementById('nonEmptyRowsNumber').textContent = nonEmptyLines.length
+    var lines = event.target.result.split(/\r?\n/);
+    var nonEmptyLines = lines.filter(isNotBlank);
+    document.getElementById('rowsNumber').textContent = lines.length;
+    document.getElementById('nonEmptyRowsNumber').textContent = nonEmptyLines.length;
     var limitAsString = document.getElementById('limit').value;
-    console.log("Limit as string: " + limitAsString)
     var limit = parseInt(limitAsString);
+    var lineNumbers = getLineNumber(nonEmptyLines);
+    var shuffledLineNumber = shuffle(lineNumbers);
+    var output = generateOutput(nonEmptyLines, shuffledLineNumber, limit);
+    var outputFileName = document.getElementById('uploadedFileName').textContent + ".limited-to-" + limit + ".csv";
 
-    console.log("Limit: " + limit)
-    console.log(typeof limit)
-
-    var lineNumbers = getLineNumber(nonEmptyLines)
-    console.log(lineNumbers)
-
-    var shuffledLineNumber = shuffle(lineNumbers)
-    console.log(shuffledLineNumber)
-
-    var output = generateOutput(nonEmptyLines, shuffledLineNumber, limit)
-
-    console.log(output)
-
-    download("output.csv", output.join("\r\n"))
+    download(outputFileName, output.join("\r\n"));
   }
 
   function generateOutput(lines, lineNumbers, limit) {
-    var result = new Array(limit)
+    var result = new Array(limit);
     for (var i = 0; i < limit; i++) {
-        result[i] = lines[lineNumbers[i]]
+        result[i] = lines[lineNumbers[i]];
     }
     return result;
   }
 
   function getLineNumber(lines) {
-    var result = new Array(lines.length);
+    var result = new Array(lines.length)
     for(i = 0; i < lines.length; i++) {
         result[i] = i;
     }
@@ -76,9 +65,9 @@ function download(filename, text) {
   function shuffle(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
+        j = Math.floor(Math.random() * (i + 1))
+        x = a[i]
+        a[i] = a[j]
         a[j] = x;
     }
     return a;
